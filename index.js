1356a7f9af922da355922328b06e3911540de4bb
seasons986 <annalin12345@gmail.com>
2023-06-26 15:28:07 -0400

    nvm

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 window.addEventListener("load", () => {
   const parent = document.getElementById("parent");
-  const text = document.getElementById("text");
-  let count = 0;
-
-  let buttons = [];
   let flowers = ["🌹","🥀","🌺","🪻","🌻","🌼","🌷","🌱","🪴","🌲","🌳","🌴","🌵","🌾","🌿","☘️","🍀", "🐸"];
 
   for (let i = 1; i < 10; i++) {
@@ -15,10 +11,5 @@ window.addEventListener("load", () => {
   function buttonClicked(e) {
     e.target.innerText = flowers[Math.floor(Math.random()*flowers.length)];
     e.target.onclick = {};
-    count++;
-    if (count >= 9) {
-      text.innerText = "🌞"
-      text.style.transform = "scale(5.4)";
-    }
   }
 });
